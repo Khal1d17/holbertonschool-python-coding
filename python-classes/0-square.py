1b11a6bcad58c8a84919606e659aa6c504e6a588
xalid <xalidlixalid056@gmail.com>
2025-06-25 07:56:54 +0000

    xalid

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+    """
+    Class atribute is important 
+       Attributes:
+       size (int): The length of a side of the square.
+    """
 class Square:
     def __init__(self, size):
         self.__size = size
