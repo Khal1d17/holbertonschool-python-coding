42eaddcac327877ab8839954acd18f8a537fa7ac
xalid <xalidlixalid056@gmail.com>
2025-06-25 09:39:36 +0000

    xalid

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,10 +6,12 @@ class Square:
     """size"""
     def __init__(self, size=0):
         self.__size = size
-
+   
+    @property
     def size(self):
         return self.__size
-
+        
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
