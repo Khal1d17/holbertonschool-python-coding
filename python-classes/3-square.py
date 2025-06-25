2281554437151f8b1155b27f39b55c7c334a33dc
xalid <xalidlixalid056@gmail.com>
2025-06-25 09:40:51 +0000

    xalid

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,10 +7,12 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
    
+
     @property
     def size(self):
         return self.__size
         
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -20,4 +22,4 @@ class Square:
         self.__size = value
 
     def area(self):
-        return self.__size**2
+        return self.__size ** 2
